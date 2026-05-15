c28989a4c4e44bdd8228bd1c2e1c095c3793a64d
constantino <cvnavarro@fi.uba.ar>
2026-05-15 19:31:12 -0300

    Copio los archivos de la rama Joaquin-development

---
+++ init_db.py
@@ -0,0 +1,24 @@
+from config import user, password, host
+import mysql.connector
+
+def init_db():
+    with open("init_db.sql") as f:
+        sql = f.read()
+
+    conn = mysql.connector.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+    )
+
+    cursor = conn.cursor()
+    for statement in sql.split(";"):
+        if statement.strip():
+            print(statement)
+            cursor.execute(statement)
+            conn.commit()
+            print("Instrucción ejecutada")
+
+    cursor.close()
+    conn.close()
