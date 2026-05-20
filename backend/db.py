755917d04e328f83751bdf95a3e1cb2f8cbfaebd
Santino <santinopereyra39@gmail.com>
2026-05-20 20:01:58 -0300

    corrijo errores

---
+++ backend/db.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import config
+
+def get_connection(): #Establece conneción con la base de datos.
+    return mysql.connector.connect(
+        host=config.host,
+        user=config.user,
+        port=3306,
+        password=config.password,
+        database=config.database
+    )
+
+
+def obtener_alumno_por_padron(padron):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    try:
+        cursor.execute(
+            "SELECT * FROM Usuarios WHERE padron = %s AND rol = 'Alumno'",
+            (padron,)
+        )
+        return cursor.fetchone()
+    finally:
+        cursor.close()
+        conn.close()
