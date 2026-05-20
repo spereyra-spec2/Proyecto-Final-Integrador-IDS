548466ab17ae60907dd3b267d787f8e816fa5cb5
Julieta Montenegro <jmontenegrol@fi.uba.ar>
2026-05-20 16:58:28 -0300

    generacion de token jwt

--- backend/src/routes/auth/auth.py
+++ backend/src/routes/auth/auth.py
@@ -8,7 +8,7 @@ auth_bp = Blueprint("auth",__name__)
 
 @auth_bp.route("/", methods = ["POST"])
 def login():
-    data = request.get_json()
+    data = request.get_json(silent=True)
 
     if not data or "padron" not in data or "contrasena" not in data:
         return errors.datos_incompletos()
--- backend/src/utils/seguridad.py
+++ backend/src/utils/seguridad.py
@@ -1,5 +1,17 @@
+from datetime import datetime, timedelta, timezone
+import jwt
 
+SECRET_KEY = "ids2026_puntoycoma"
 
-def generar_token():
-    #falta completar
-    print()
+def generar_token(usuario):
+    payload = {
+        'iat': datetime.now(tz=timezone.utc),
+        'exp': datetime.now(tz=timezone.utc) + timedelta(hours=4),
+        'padron': usuario["padron"],
+        'rol': usuario["rol"]
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+def verify_token(headers):
+    #falta implementar con rol Docente
+    return False
