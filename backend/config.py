42a012812c870d9a5be166764f389cd21a2c4a1b
FranciscoCasais <franjcasais@gmail.com>
2026-05-20 23:35:36 -0300

    Más variables globales

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,10 @@
 host: str = "localhost"
 user: str = "root"
-password: str = 
+password: str = "root"
 database: str = "ids_db"
+MODEL_PATH: str = "src/models/init_db.sql"
+
+URL_BASE: str = "api"
+
+# En producción no debería estar hardcodeada; usar variable de entorno o similar (?)
+SECRET_KEY = "ids2026_puntoycoma"
