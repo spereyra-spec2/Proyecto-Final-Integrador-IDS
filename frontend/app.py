f41c656c291ba5ae55a529aec1a16f52d415bcf1
Emanuel <ejpena@fi.uba.ar>
2026-05-30 19:15:34 -0300

    Feature(frontend): se agregaron los templates relacionados a /evaluaciones

---
+++ frontend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='static')
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('profesor-evaluaciones.html')
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
