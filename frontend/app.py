b9fec242679799bc28996d8b7fa33b9cc8e5cab0
Julieta Montenegro <jmontenegrol@fi.uba.ar>
2026-06-03 19:22:15 -0300

    borro comentario de version anterior

--- frontend/app.py
+++ frontend/app.py
@@ -11,9 +11,6 @@ app = Flask(__name__,
 CORS(app)
 app.config['SECRET_KEY'] = 'clave_secreta_ids_2026'
 
-# @app.route('/')
-# def index():
-#     return render_template('alumno-inicio.html')
 
 app.register_blueprint(auth_bp, url_prefix="/api/auth") #sigo ejemplo del repo de cátedra
 app.register_blueprint(profesor_bp, url_prefix="/api/profesor")
