579fe19047b87f8be0f740d99238ec75a193dd1f
Emanuel <ejpena@fi.uba.ar>
2026-06-03 21:54:38 -0300

    Actualiza estructura de base de datos y modelos

--- backend/src/models/init_db.sql
+++ backend/src/models/init_db.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE IF NOT EXISTS ids_db;
 USE ids_db;
-<<<<<<< HEAD
 
 CREATE TABLE IF NOT EXISTS Curso (
     idCurso INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -24,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Curso_has_Usuarios (
     cursando_actualmente TINYINT,
     FOREIGN KEY (Curso_idCurso) REFERENCES Curso(idCurso) ON DELETE CASCADE,
     FOREIGN KEY (Usuarios_padron) REFERENCES Usuarios(padron) ON DELETE CASCADE
-);
+    );
 
 CREATE TABLE IF NOT EXISTS Asistencias (
     idAsistencia INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -97,49 +96,3 @@ CREATE TABLE IF NOT EXISTS Logs (
     Usuarios_padron INT NOT NULL,
     FOREIGN KEY (Usuarios_padron) REFERENCES Usuarios(padron)
 );
-=======
-CREATE TABLE IF NOT EXISTS usuarios (
-    padron INT NOT NULL PRIMARY KEY,
-    rol ENUM('estudiantes', 'docente') NOT NULL,
-    nombres VARCHAR(255),
-    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
-    contrasena_hash VARCHAR(255) NOT NULL,
-    cursando_actualmente BOOLEAN DEFAULT FALSE,
-    mail VARCHAR(255) UNIQUE NOT NULL,
-    grupo_ID INT
-);
-CREATE TABLE IF NOT EXISTS grupos (
-    grupo_ID INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL
-);
-ALTER TABLE usuarios ADD FOREIGN KEY (grupo_ID) REFERENCES grupos(grupo_ID) ON DELETE SET NULL;
-CREATE TABLE IF NOT EXISTS cursos (
-    curso_id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_curso VARCHAR(255) NOT NULL,
-    descripcion TEXT
-);
-CREATE TABLE IF NOT EXISTS evaluaciones (
-    evaluacion_ID INT AUTO_INCREMENT PRIMARY KEY,
-    tipo VARCHAR(255),
-    descripcion VARCHAR(255),
-    fecha DATETIME,
-    curso_id INT,
-    FOREIGN KEY (curso_id) REFERENCES cursos(curso_id) ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS notas (
-    alumno_padron INT NOT NULL,
-    evaluacion_ID INT NOT NULL,
-    nota DECIMAL(5,2),
-    PRIMARY KEY (alumno_padron, evaluacion_ID),
-    FOREIGN KEY (alumno_padron) REFERENCES usuarios(padron) ON DELETE CASCADE,
-    FOREIGN KEY (evaluacion_ID) REFERENCES evaluaciones(evaluacion_ID) ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS asistencias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    asistio BOOLEAN DEFAULT FALSE,
-    fecha DATE NOT NULL,
-    padron INT NOT NULL,
-    justificado BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (padron) REFERENCES usuarios(padron) ON DELETE CASCADE
-);
->>>>>>> emanuel_dev
