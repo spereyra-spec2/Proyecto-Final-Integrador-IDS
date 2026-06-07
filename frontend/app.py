cb5a7f0fd8cb3d5c0fb7f02d56a423800cf04279
Emanuel <ejpena@fi.uba.ar>
2026-06-07 19:36:15 -0300

    se agrego la app.route (evaluaciones) a app.py de Frontend

--- frontend/app.py
+++ frontend/app.py
@@ -17,7 +17,7 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return render_template('profesor-evaluaciones.html')
+    return render_template('index.html')
 
 @app.route("/login")
 def sin_autorizacion():
@@ -119,6 +119,11 @@ def obtener_asistencia_padron(padron):
         print(f"Error de conexión con el Back: {e}")
         datos_asistencia = [] 
     return render_template("asistencia_padron.html", asistencias=datos_asistencia)
+
+@app.route('/evaluaciones', methods=['GET', 'POST'])
+def evaluaciones():
+    return render_template('profesor-evaluaciones.html')
+
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
